6bc307a940f711889268fb135c670525e3b0bcf5
Mohamed Sameh <ms627673@gmail.com>
2020-07-17 15:27:16 +0200

    added free trial

--- js/main.js
+++ js/main.js
@@ -1,6 +1,75 @@
 jQuery(document).ready(function($) {
     'use strict';
     // This is needed if the user scrolls down during page load and you want to make sure the page is scrolled to the top once it's fully loaded. This has Cross-browser support.
+    // backgroundColor: [
+    //     '#00FFDB',
+    //     '#387DFD',
+    //     '#00FFDB',
+    //     '#FF5A4E',
+    //     '#00FFDB',
+    //     '#387DFD',
+    //     '#387DFD',
+    //     '#FF5A4E',
+    // ],
+    var ctx = document.getElementById('myChart');
+    if (ctx) {
+
+        var stackedBar = new Chart(ctx, {
+            type: 'bar',
+            data: {
+                datasets: [{
+                    label: 'first',
+                    data: [10, 20, 30, 40],
+                    backgroundColor: [
+                        // '#00FFDB',
+                        // '#387DFD',
+                        // '#00FFDB',
+                        // '#FF5A4E',
+                        '#00FFDB',
+                        '#387DFD',
+                        '#387DFD',
+                        '#FF5A4E',
+                    ],
+                }, {
+                    label: 'second',
+                    data: [50, 60, 70, 80],
+                    backgroundColor: [
+                        '#00FFDB',
+                        '#387DFD',
+                        '#00FFDB',
+                        '#FF5A4E',
+                        // '#00FFDB',
+                        // '#387DFD',
+                        // '#387DFD',
+                        // '#FF5A4E',
+                    ],
+                    borderWidth: 1
+                }],
+                labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                options: {
+                    legend: {
+                        display: false
+                    },
+                    tooltips: {
+                        callbacks: {
+                            label: function(tooltipItem) {
+                                return tooltipItem.yLabel;
+                            }
+                        }
+                    },
+                    scales: {
+                        xAxes: [{
+                            ticks: {
+                                beginAtZero: true
+                            }
+                        }]
+                    }
+
+                }
+            },
+        });
+    }
+
     if ('scrollRestoration' in history) {
         history.scrollRestoration = 'manual';
     }
@@ -164,4 +233,143 @@ jQuery(document).ready(function($) {
         navbarOverlay.fadeOut();
         body.css('cursor', 'default');
     }
+
+    //jQuery time
+    var current_fs, next_fs, previous_fs; //fieldsets
+    var left, opacity, scale; //fieldset properties which we will animate
+    var animating; //flag to prevent quick multi-click glitches
+    $(".next").click(function() {
+        if (!$(this).hasClass('last')) {
+
+            if (animating) return false;
+            animating = true;
+
+            current_fs = $(this).parent().parent();
+            next_fs = $(this).parent().parent().next();
+
+            //activate next step on progressbar using the index of next_fs
+            // $(".progressbar li").eq($("fieldset").index(next_fs)).addClass("active");
+
+            //show the next fieldset
+            next_fs.show();
+            //hide the current fieldset with style
+            current_fs.animate({ opacity: 0 }, {
+                step: function(now, mx) {
+                    //as the opacity of current_fs reduces to 0 - stored in "now"
+                    //1. scale current_fs down to 80%
+                    scale = 1 - (1 - now) * 0.2;
+                    //2. bring next_fs from the right(50%)
+                    left = (now * 50) + "%";
+                    //3. increase opacity of next_fs to 1 as it moves in
+                    opacity = 1 - now;
+                    current_fs.css({
+                        'transform': 'scale(' + scale + ')',
+                        'position': 'absolute'
+                    });
+                    next_fs.css({ 'left': left, 'opacity': opacity });
+                },
+                duration: 800,
+                complete: function() {
+                    current_fs.hide();
+                    animating = false;
+                },
+                //this comes from the custom easing plugin
+                easing: 'easeInOutBack'
+            });
+
+            if ($(".progressbar li")) {
+                $(".progressbar li").eq($("fieldset").index(current_fs)).find('span').animate({ width: "100%", }, 1000, function() {
+                    setTimeout(() => {
+                        $(".progressbar li").eq($("fieldset").index(current_fs) + 1).addClass('active');
+                    }, 200);
+                });
+            }
+        }
+    });
+
+    $(".prev-stage1").click(function() {
+        if (animating) return false;
+        animating = true;
+
+        current_fs = $(this).parent().parent();
+        previous_fs = $(this).parent().parent().prev();
+
+        //de-activate current step on progressbar
+
+        //show the previous fieldset
+        previous_fs.show();
+        //hide the current fieldset with style
+        current_fs.animate({ opacity: 0 }, {
+            step: function(now, mx) {
+                //as the opacity of current_fs reduces to 0 - stored in "now"
+                //1. scale previous_fs from 80% to 100%
+                scale = 0.8 + (1 - now) * 0.2;
+                //2. take current_fs to the right(50%) - from 0%
+                left = ((1 - now) * 50) + "%";
+                //3. increase opacity of previous_fs to 1 as it moves in
+                opacity = 1 - now;
+                current_fs.css({ 'left': left });
+                previous_fs.css({ 'transform': 'scale(' + scale + ')', 'opacity': opacity });
+            },
+            duration: 800,
+            complete: function() {
+                current_fs.hide();
+                animating = false;
+                previous_fs.css({ 'position': 'relative' });
+            },
+            //this comes from the custom easing plugin
+            easing: 'easeInOutBack'
+        });
+        $(".progressbar li").eq($("fieldset").index(current_fs)).removeClass('active', function() {
+            $(".progressbar li").eq($("fieldset").index(current_fs) - 1).find('span').animate({ width: "0", }, 1000);
+        });
+    });
+
+    $(".submit").click(function() {
+        return false;
+    });
+
+    function mySubmitFunction(e) {
+        e.preventDefault();
+        someBug();
+        return false;
+    }
+
+    function signInCallback(authResult) {
+        if (authResult['code']) {
+
+            // Hide the sign-in button now that the user is authorized, for example:
+            $('#signinButton').attr('style', 'display: none');
+
+            // Send the code to the server
+            $.ajax({
+                type: 'POST',
+                url: 'http://localhost/kisocial',
+                // Always include an `X-Requested-With` header in every AJAX request,
+                // to protect against CSRF attacks.
+                headers: {
+                    'X-Requested-With': 'XMLHttpRequest'
+                },
+                contentType: 'application/octet-stream; charset=utf-8',
+                success: function(result) {
+                    // Handle or verify the server response.
+                },
+                processData: false,
+                data: authResult['code']
+            });
+            console.log(authResult['code']);
+
+        } else {
+            // There was an error.
+        }
+    }
+    $('#signinButton').click(function() {
+        // signInCallback defined in step 6.
+        auth2.grantOfflineAccess().then(signInCallback);
+        // auth2.signIn({ ux_mode: 'redirect' });
+    });
+    $('.selectbox select').on('change', function() {
+        $(this).parent().find('input').attr('placeholder', '+ ' + $(this).find('option:selected').val() + ' Telephone');
+    });
+
 });
